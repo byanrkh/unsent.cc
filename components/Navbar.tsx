48dbbdaeea5e4d1c9d798088e595ab50311257c1
byanrkh <abyan.raditya@icloud.com>
2026-09-22 21:47:37 +0700

    Mod: re arrange navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="w-full px-6 pt-8 pb-8 sm:px-10 sm:pt-10 sm:pb-9">
-      <div className="mx-auto flex max-w-175 items-center justify-between text-sm sm:text-base">
+      <div className="mx-auto flex max-w-175 items-center justify-between text-sm">
+        <Link
+          href="/"
+          className="tracking-wide text-lg text-[#171717] transition-opacity hover:opacity-55"
+        >
+          unsent.cc
+        </Link>
         <div className="flex items-center gap-5 sm:gap-8">
           <Link
             href="/explore"
@@ -18,12 +24,6 @@ export default function Navbar() {
             Terms
           </Link>
         </div>
-        <Link
-          href="/login"
-          className="tracking-wide text-[#171717] transition-opacity hover:opacity-55"
-        >
-          [ Login ]
-        </Link>
       </div>
     </nav>
   );
